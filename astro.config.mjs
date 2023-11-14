799b712f9b66540055f3400e02db3cbe74df7ab3
LAMAS38 <LAMAS38@ulaval.ca>
2023-11-14 00:35:55 -0500

    ajustement de la base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://lamas38.github.io',
-    base: '/portfolio',
+    site: 'https://lamas38.github.io/portfolio',
+    // base: '',
 });
