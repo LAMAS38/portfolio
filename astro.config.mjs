810173edb154c5f0bedc724e9877181d516c5cb7
LAMAS38 <LAMAS38@ulaval.ca>
2023-11-14 00:38:55 -0500

    modif

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lamas38.github.io/portfolio',
-    // base: '',
+    base: '/portfolio',
 });
